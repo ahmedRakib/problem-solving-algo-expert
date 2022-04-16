d08a351142894d6c9c369c9401f64e0225867b63
Mohammad Rakibul Hasan <rkhasan333@gmail.com>
2022-04-16 02:03:18 +0200

    another optimal solution of two number sum using array sort

--- 01 Easy/TwoNumberSum/Program.cs
+++ 01 Easy/TwoNumberSum/Program.cs
@@ -7,34 +7,64 @@ namespace TwoNumberSum
     {
         static void Main(string[] args)
         {
-            //int[] nums = new int[] { 3, 2, 4 };
 
-           
+            int[] nums = new int[] { 3,2,4 };
 
-            int[] nums = new int[] { 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1 };
+            //int[] nums = new int[] { 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1 };
             int[] result = new int[2];
-            var tempResult = new Dictionary<int, int>();
-            tempResult.Add(nums[0], 0);
+           
+            int target = 6;
 
-            int target = 11;
+            #region using hashmap
+            //var tempResult = new Dictionary<int, int>();
+            //tempResult.Add(nums[0], 0);
+            //for (int i = 1; i < nums.Length; i++)
+            //{
+            //    int temp = target - nums[i];
+            //    if (tempResult.ContainsKey(temp))
+            //    {
+            //        tempResult.TryGetValue(temp, out result[0]);
+            //        result[1] = i;
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        if (!tempResult.ContainsKey(nums[i]))
+            //        {
+            //            tempResult.Add(nums[i], i);
+            //        }
+            //    }
+            //}
+            #endregion
 
-            for (int i = 1; i < nums.Length; i++)
+            #region using sorting and pointer
+            //to keep track of the index of original array
+            int[] clonedNums = (int[])nums.Clone();
+            Array.Sort(clonedNums);
+            int x = 0; int y = nums.Length - 1;
+
+            for (int i = 0; i < nums.Length; i++)
             {
-                int temp = target - nums[i];
-                if(tempResult.ContainsKey(temp))
+                int tempSum = clonedNums[x] + clonedNums[y];
+                if (tempSum == target)
                 {
-                    tempResult.TryGetValue(temp, out result[0]);
-                    result[1] = i;
+                    result[0] = Array.IndexOf(nums, clonedNums[x]);
+                    //if the array contains same value, then to restrict to return the same index again 
+                    nums[result[0]] = int.MinValue;
+                    result[1] = Array.IndexOf(nums, clonedNums[y]);
+                    nums[result[1]] = int.MinValue;
                     break;
                 }
-                else
+                else if (tempSum < target)
                 {
-                    if(!tempResult.ContainsKey(nums[i]))
-                    {
-                        tempResult.Add(nums[i], i);
-                    }
+                    x++;
+                }
+                else if (tempSum > target)
+                {
+                    y--;
                 }
             }
+            #endregion
 
             Console.WriteLine("[" + result[0].ToString() + " , " + result[1].ToString() + "]");
         }
